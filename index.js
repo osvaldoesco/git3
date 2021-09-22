e6d6c05e688342138fe987996cdd50249a60a76c
Osvaldo Escobar <eoeg1410@gmail.com>
2021-09-22 16:39:08 -0600

    Add 0 to the list

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const sum = (a, b) => {
  return a + b;
 };
 
+console.log(`Resutlado de 0 + 0 =`, sum(0,0));
 console.log(`Resutlado de 2 + 2 =`, sum(2,2));
 console.log(`Resutlado de 3 + 3 =`, sum(3,3));
 console.log(`Resutlado de 4 + 5 =`, sum(5,5));
