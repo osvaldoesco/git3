d0639f0eff89c96163d616b35a97eec9dd001fc5
Osvaldo Escobar <eoeg1410@gmail.com>
2021-09-22 16:30:39 -0600

    Add new sum results

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('git');
+
+
+const sum = (a, b) => {
+ return a + b;
+};
+
+console.log(`Resutlado de 2 + 2 =`, sum(2,2));
+console.log(`Resutlado de 3 + 3 =`, sum(3,3));
+console.log(`Resutlado de 4 + 5 =`, sum(5,5));
+console.log(`Resutlado de 5 + 5 =`, sum(5,5));
