30fb94d2988547872c76cb00f1e51a7dd4077f31
Osvaldo Escobar <eoeg1410@gmail.com>
2021-09-22 16:37:13 -0600

    Add 1 and 6 to the list

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const sum = (a, b) => {
  return a + b;
 };
 
+console.log(`Resutlado de 1 + 1 =`, sum(1,1));
 console.log(`Resutlado de 2 + 2 =`, sum(2,2));
 console.log(`Resutlado de 3 + 3 =`, sum(3,3));
 console.log(`Resutlado de 4 + 5 =`, sum(5,5));
 console.log(`Resutlado de 5 + 5 =`, sum(5,5));
+console.log(`Resutlado de 5 + 5 =`, sum(6,6));
